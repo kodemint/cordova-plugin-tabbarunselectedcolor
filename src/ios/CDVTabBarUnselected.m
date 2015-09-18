1addee9863a7b285eac22e8e0e60a01e380cff22
Premith <premith@kodemint.com>
2015-09-18 14:47:33 +0530

    Hex String to Color Fix

--- src/ios/CDVTabBarUnselected.m
+++ src/ios/CDVTabBarUnselected.m
@@ -39,13 +39,14 @@
     }
 }
 
-- (UIColor*)colorStringToColor:(NSString*)colorStr
+
+- (UIColor*)colorStringToColor:(NSString*)hexString
 {
-    NSArray *rgba = [colorStr componentsSeparatedByString:@","];
-    return [UIColor colorWithRed:[[rgba objectAtIndex:0] intValue]/255.0f
-                           green:[[rgba objectAtIndex:1] intValue]/255.0f
-                            blue:[[rgba objectAtIndex:2] intValue]/255.0f
-                           alpha:[[rgba objectAtIndex:3] intValue]/255.0f];
+    unsigned rgbValue = 0;
+    NSScanner *scanner = [NSScanner scannerWithString:hexString];
+    [scanner setScanLocation:1]; // bypass '#' character
+    [scanner scanHexInt:&rgbValue];
+    return [UIColor colorWithRed:((rgbValue & 0xFF0000) >> 16)/255.0 green:((rgbValue & 0xFF00) >> 8)/255.0 blue:(rgbValue & 0xFF)/255.0 alpha:1.0];
 }
 
 @end
